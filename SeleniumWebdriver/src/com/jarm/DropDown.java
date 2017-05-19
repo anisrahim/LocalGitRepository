f33b6123abf153d6cc0aad4f852d74c59e66f47c
anisrahim <anis_rahim@yahoo.com>
2017-05-19 07:17:16 -0400

    Update DropDown.java

--- SeleniumWebdriver/src/com/jarm/DropDown.java
+++ SeleniumWebdriver/src/com/jarm/DropDown.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DropDown {
 	public static void main(String[] args) {
